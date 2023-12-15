05658a88d10082b1e60b3e6f7d55003f6ffa8c65
vyshnav <vyshnavravi599@gmail.com>
2023-12-15 16:01:35 +0530

    Update placement routes import

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 let PORT = process.env.PORT || 3000
-const placementUpdate = require('./routes/routes')
+const placementUpdate = require('./routes/palcement_routes')
 
 
 app.use('/api/placement', placementUpdate)
