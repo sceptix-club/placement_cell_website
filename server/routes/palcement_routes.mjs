2f74e7253985e657d4697f828f1f9de959061ec8
vyshnav <vyshnavravi599@gmail.com>
2023-12-17 23:43:09 +0530

    Refactor placement_routes.mjs

--- server/routes/palcement_routes.mjs
+++ server/routes/palcement_routes.mjs
@@ -1,16 +1,13 @@
-import express from 'express'
-const app = express()
-import 'dotenv/config'
-import bodyParser from 'body-parser';
-app.use(bodyParser.json())
-const router = express.Router()
-import { addPlacement, createJwt } from '../controller/controller.mjs'
-import verifyJwtToken from '../middleware/verify_jwt_token.mjs'
+import express from "express";
+const app = express();
+import "dotenv/config";
+import bodyParser from "body-parser";
+app.use(bodyParser.json());
+const router = express.Router();
+import { addPlacement, createJwt } from "../controller/controller.mjs";
+import verifyJwtToken from "../middleware/verify_jwt_token.mjs";
 
+router.get("/", createJwt);
+router.post("/add", verifyJwtToken, addPlacement);
 
-
-router.get('/',createJwt)
-router.post('/add',verifyJwtToken, addPlacement)
-
-
-export default router
+export default router;
