58e213547b192b77512b29f3bd5f5638dfdbd8d1
MacDsouza <120902232+MacDsouza@users.noreply.github.com>
2023-12-23 15:53:41 +0530

    header mobile view and footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>This is the footer component.</p>
+      <hr className="border-t-2 border-gray-300" />
+      <div className="flex items-center justify-center w-full lg:h-8 px-auto bg-transparent">
+        <a href="https://www.sjec.ac.in/" target="_blank" rel="noopener noreferrer" className="lg:text-2xl text-xl font-bold text-white text-center w-full hover:underline">ST JOSEPH ENGINEERING COLLEGE</a>
+      </div>
+      <div className="flex justify-between px-4 py-2 bg-transparent">
+        <p className="lg:pl-4 pl-2 text-white text-xs">&#169; 2023 All rights reserved.</p>
+        <p className="lg:pr-4 pl-2 text-white text-xs">Powered by Sceptix</p>
+      </div>
     </footer>
   );
 };
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -9,9 +9,8 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    
     <div className="flex justify-between lg:h-38 w-full items-center p-4 bg-transparent-500 text-white ">
-      <h1 className="pl-2 font-serif text-2xl font-bold text-white">PLACEMENT_WEBSITE</h1>
+      <h1 className="pl-2 font-serif lg:text-2xl text-2xl font-bold text-white">PLACEMENT_WEBSITE</h1>
       <div className="relative pr-3">
         <img 
           src={Image} alt="." className="h-6 cursor-pointer" onClick={() => setIsOpen(!isOpen)}
@@ -20,16 +19,16 @@ const Header = () => {
           <>
             {LoggedIn ? (
         <>
-          <div className="absolute right-0 mt-2 w-48 h-44  bg-white rounded-md overflow-hidden shadow-xl z-10">
-          <h3 className='text-2xl text-black  font-bold text-center py-4 '>Hi {UserName} </h3>
-          <a href="#" className=" block px-4 py-3 text-lg text-gray-700 hover:bg-blue-500 hover:text-white">Profile</a>
-          <a href="#" className=" block px-4 py-4 text-lg text-gray-700 hover:bg-blue-500 hover:text-white">Log out</a>
+          <div className="absolute right-0 mt-2 lg:w-48 w-40 lg:h-44  bg-white rounded-md overflow-hidden shadow-xl z-10">
+          <h3 className='text-xl lg:text-2xl text-black  font-bold text-center py-4 '>Hi {UserName} </h3>
+          <a href="#" className=" block px-4 py-3 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Profile</a>
+          <a href="#" className=" block px-4 py-4 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Log out</a>
           </div>
         </>
       ) : (
         <>
           <div className="absolute right-0 mt-2 w-48 h-30  bg-white rounded-md overflow-hidden shadow-xl z-10">
-          <h3 className='text-3xl text-black  font-bold text-center py-4'>Hello</h3>
+          <p className='text-3xl text-black  font-bold text-center py-4'>Hello</p>
           <a href="#" className=" text-center block px-4 py-2 text-xl text-gray-700 hover:bg-blue-500 hover:text-white">Log in</a>
           </div>
         </>
