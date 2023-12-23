956142cda10a920b5e9240f753fad3d7c93ef630
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2023-12-23 21:47:16 +0530

    commit

--- client/src/app/candidates/page.jsx
+++ client/src/app/candidates/page.jsx
@@ -3,43 +3,7 @@ import React from "react";
 const candidates = () => {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-white p-8 rounded shadow-md w-96">
-          <h2 className="text-2xl font-bold mb-6">Login</h2>
-          <form>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                Username
-              </label>
-              <input
-                className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter your username"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-              />
-            </div>
-            <button
-              className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
-              type="submit"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
+
     </>
   );
 };
