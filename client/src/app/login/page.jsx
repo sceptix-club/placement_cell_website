faa89130b8c5605c8b505961a8bdb815aea65196
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2023-12-23 17:58:35 +0530

    commit

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -5,6 +5,7 @@ const login = () => {
   return (
     <>
 
+
       <div className="flex items-center  justify-center h-screen bg-gradient-radial bg-background-clr">
 
 
@@ -24,9 +25,10 @@ const login = () => {
                   placeholder="Enter your username"
                 />
               </div>
-              <div className="flexmb-4">
-                <label htmlFor="password" className="flex  block text-gray-700 text-sm  mb-2">
-                  Password<Link href="/forgot" className="text-green-500 font-normal mr-8 px-10">Forgot password?</Link>
+              <div className="mb-4">
+                <label htmlFor="password" className="flex justify-between mr-2  block text-gray-700 text-sm  mb-2">
+                  Password
+                  <Link href="/forgot" className="text-green-500 font-normal mr-8 px-10">Forgot password?</Link>
                 </label>
 
                 <input
@@ -41,15 +43,15 @@ const login = () => {
               <div className="flex items-center py-3 justify-between">
                 <button
                   type="submit"
-                  className="bg-green-700 py-2 text-white w-full rounded-md hover:bg-green-700 transition-all"
+                  className="bg-green-700 py-2 text-white w-full rounded-md hover:bg-green-700 transition-all hover:shadow-xl"
                 >
                   Login
                 </button>
 
               </div>
               <div className="flex px-9 text-sm text-black py-3">
-                <span className="mr-4">Not a Student?</span>
-                <Link href="/Mentor" className="text-green-500">Sign in as a Mentor</Link>
+
+                <Link href="/Mentor" className="text-green-500 font-normal px-14">Sign in as a Mentor</Link>
               </div>
             </form>
           </div>
