0a2f4cba981106d75b65c1de083dacf8ec41f6ae
vyshnav <vyshnavravi599@gmail.com>
2023-12-25 11:26:11 +0530

    Remove createJwt function from controller

--- server/controller/controller.mjs
+++ server/controller/controller.mjs
@@ -25,13 +25,6 @@ export const addPlacement = async (req, res) => {
     }
 };
 
-export const createJwt = (req, res) => {
-    let jwt_token = Jwt.sign(process.env.MANAGER_SECRET_KEY, process.env.MANAGER_SECRET_KEY);
-
-    res.setHeader("Set-Cookie", `jwt_token=${jwt_token}; Path=/; HttpOnly`);
-    res.status(200).send({ token: jwt_token });
-    return jwt_token;
-};
 
 export const getDrives = (req, res) => {
     fs.readFile("data.json", "utf-8", (err, data) => {
