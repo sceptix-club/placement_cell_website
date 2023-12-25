a1f211264874e295f685b930508a006ee55126be
MacDsouza <120902232+MacDsouza@users.noreply.github.com>
2023-12-25 23:39:09 +0530

    Modified header to satisfy login conditions

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,12 +1,21 @@
 "use client";
 import React, { useState } from 'react'; 
+// import { useRouter } from 'next/navigation';
 import Image from "next/image";
+import { usePathname } from 'next/navigation'
 
-const UserName="Vyasa";
-const LoggedIn=true;
+const UserName = "Vyasa";
+const LoggedOut = false;
+const ImageSource = "/testimg.png";
 
+
+//loginpg-no circle
+//home pg only login circle
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const PathName = usePathname();
+  console.log(PathName);
 
   return (
     <div className="flex justify-between lg:h-38 w-full items-center p-4 bg-transparent-500 text-white font-inter">
@@ -18,31 +27,55 @@ const Header = () => {
               width={100}
               height={24}
               priority className="w-12 h-12 rounded-full object-cover dark:invert cursor-pointer" onClick={() => setIsOpen(!isOpen)}
-        />
+        /> 
          {isOpen && ( 
           <>
-            {LoggedIn ? (
-        <>
+          { PathName === '/login'?
+            (null)
+            :
+            (( PathName === '/' && LoggedOut )?(
+              <>
+              <div className="absolute right-0 mt-2 w-48 h-30  bg-white rounded-md overflow-hidden shadow-xl z-10">
+                <p className='text-3xl text-black  font-bold text-center py-3'>Hello</p>
+                <a href="#" className=" text-center block px-4 py-3 text-xl text-gray-700 hover:bg-blue-500 hover:text-white">Log in</a>
+              </div>
+              </>
+            )
+            :
+            (
+              <div className="absolute right-0 mt-2 lg:w-48 w-40 lg:h-44  bg-white rounded-md overflow-hidden shadow-xl z-10">
+                <h3 className='text-xl lg:text-2xl text-black  font-bold text-center py-4 '>Hi {UserName} </h3>
+                <a href="#" className=" block px-4 py-3 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Profile</a>
+                <a href="#" className=" block px-4 py-4 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Log out</a>
+              </div>
+            )
+            )
+          }
+
+
+
+            {/* {LoggedIn ? (
           <div className="absolute right-0 mt-2 lg:w-48 w-40 lg:h-44  bg-white rounded-md overflow-hidden shadow-xl z-10">
           <h3 className='text-xl lg:text-2xl text-black  font-bold text-center py-4 '>Hi {UserName} </h3>
           <a href="#" className=" block px-4 py-3 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Profile</a>
           <a href="#" className=" block px-4 py-4 lg:text-lg text-l text-gray-700 hover:bg-blue-500 hover:text-white">Log out</a>
           </div>
-        </>
+        
       ) : (
-        <>
+        
           <div className="absolute right-0 mt-2 w-48 h-30  bg-white rounded-md overflow-hidden shadow-xl z-10">
           <p className='text-3xl text-black  font-bold text-center py-3'>Hello</p>
           <a href="#" className=" text-center block px-4 py-3 text-xl text-gray-700 hover:bg-blue-500 hover:text-white">Log in</a>
           </div>
-        </>
-      )}
+        
+      )} */}
           </>
         )} 
       </div>
     </div>
   );
 };
+
 
 // function DropdownItem(props){
 //   return(
