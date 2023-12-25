0f5c85ce21f585f175848c3d1395d9984f7f7c9a
vyshnav <vyshnavravi599@gmail.com>
2023-12-25 11:07:28 +0530

    Add placement data and set JWT token in response

--- server/controller/controller.mjs
+++ server/controller/controller.mjs
@@ -9,6 +9,7 @@ import fs from "fs";
 // app.use(bodyParser.json());
 
 export const addPlacement = async (req, res) => {
+    
     // res.status(200).send({ message: `you got access you added ${req.body.company} placement drive` })
     try {
         const json = JSON.stringify(req.body);
@@ -17,7 +18,7 @@ export const addPlacement = async (req, res) => {
                 console.log(err);
                 return;
             }
-            console.log("updated");
+            res.status(200).send({message:"Data added "})
         });
     } catch (err) {
         throw err;
@@ -25,8 +26,11 @@ export const addPlacement = async (req, res) => {
 };
 
 export const createJwt = (req, res) => {
-    let jwt_token = Jwt.sign(process.env.MANAGER_SECRET_KEY, process.env.MANAGER_SECRET_KEY, {});
+    let jwt_token = Jwt.sign(process.env.MANAGER_SECRET_KEY, process.env.MANAGER_SECRET_KEY);
+
+    res.setHeader("Set-Cookie", `jwt_token=${jwt_token}; Path=/; HttpOnly`);
     res.status(200).send({ token: jwt_token });
+    return jwt_token;
 };
 
 export const getDrives = (req, res) => {
@@ -35,7 +39,6 @@ export const getDrives = (req, res) => {
             res.send({ error: err });
             return;
         }
-
         res.send({ data });
     });
 };
