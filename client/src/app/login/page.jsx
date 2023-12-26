104c8940a2af25d78679e39b8b3060a03203a1fd
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2023-12-26 18:25:04 +0530

    added invalid credentials

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -5,13 +5,31 @@ import Link from 'next/link'
 const login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const handleLogin = () => {
+    // Basic validation for empty fields
+    if (!email || !password) {
+      setError('Please enter both username and password.');
+      return;
+    }
+    console.log('Email:', email);
+    console.log('Password:', password);
 
-    // Add your authentication logic here (e.g., connecting to a backend)
-    console.log('Logging in with:', { email, password });
+    // Additional login logic can be added here
+
+    // Clear error if there was any
+    setError('');
   };
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleLogin();
+    }
+  };
+
+  // Add your authentication logic here (e.g., connecting to a backend)
+  //   console.log('Logging in with:', { email, password });
+  // };
 
 
 
@@ -32,18 +50,20 @@ const login = () => {
         <div className="bg-white p-10 rounded shadow-md w-96 hover-shadow">
           <h2 className="text-2xl font-semibold mb-6  text-black text-center">Login to your account</h2>
 
+
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-600 text-sm font-bold mb-2">Email</label>
-            <input type="email" id="email" className="w-full border-2 border-black rounded py-2 px-3 text-sm text-black " value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+            <input type="email" id="email" className="w-full border-2 border-black rounded py-2 px-3 text-sm text-black " onKeyPress={handleKeyPress} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
           </div>
 
           <div className="mb-4 relative">
             <label htmlFor="password" className="block text-gray-600 text-sm font-bold mb-2">Password</label>
-            <input type="password" id="password" className="w-full  border-2 border-black rounded py-2 px-3 text-sm text-black" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+            <input type="password" id="password" className="w-full  border-2 border-black rounded py-2 px-3 text-sm text-black" onKeyPress={handleKeyPress} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
             <Link href="/forgot" className="text-green-500 text-sm absolute top-0 right-0 mt-1 mr-1 font-bold  ">Forgot Password?</Link>
           </div>
 
           <button type="submit" className=" text-white py-2 px-4 rounded w-full mb-4 bg-gradient-to-r from-[#199252] to-[#199252] " onClick={handleLogin}>Login</button>
+          {error && <p className="text-red-500 mb-4">{error}</p>}
 
 
 
