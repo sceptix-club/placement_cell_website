d2797e2d6f17fa630f270db8a1cdb74cba864397
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2023-12-28 18:33:32 +0530

    added regex

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -6,24 +6,52 @@ const login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isValidCredentials, setIsValidCredentials] = useState(true);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsValidCredentials(true); // Reset validation message on input change
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setIsValidCredentials(true); // Reset validation message on input change
+  };
+
+  const validateCredentials = () => {
+    // Basic email validation using regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Password must be at least 6 characters
+    const passwordRegex = /^.{6,}$/;
+
+    const isValidEmail = emailRegex.test(email);
+    const isValidPassword = passwordRegex.test(password);
+
+    return isValidEmail && isValidPassword;
+  };
 
   const handleLogin = () => {
-    // Basic validation for empty fields
-    if (!email || !password) {
-      setError('Please enter both username and password.');
+    const isValid = validateCredentials();
+
+    if (email.trim() === '' || password.trim() === '') {
+      setIsValidCredentials(false);
       return;
     }
-    console.log('Email:', email);
-    console.log('Password:', password);
 
-    // Additional login logic can be added here
-
-    // Clear error if there was any
-    setError('');
+    if (isValid) {
+      // Perform login logic here
+      console.log('Login successful!');
+      // You can redirect to another page or perform other actions upon successful login
+    } else {
+      setIsValidCredentials(false);
+    }
   };
+
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      handleLogin();
+      console.log('Email:', email);
+      console.log('Password:', password);
     }
   };
 
@@ -53,12 +81,12 @@ const login = () => {
 
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-600 text-sm font-bold mb-2">Email</label>
-            <input type="email" id="email" className="w-full border-2 border-black rounded py-2 px-3 text-sm text-black " onKeyPress={handleKeyPress} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+            <input type="email" id="email" className="w-full border-2 border-black rounded py-2 px-3 text-sm text-black " onKeyPress={handleKeyPress} value={email} onChange={handleEmailChange} placeholder="Enter your email" />
           </div>
 
           <div className="mb-4 relative">
             <label htmlFor="password" className="block text-gray-600 text-sm font-bold mb-2">Password</label>
-            <input type="password" id="password" className="w-full  border-2 border-black rounded py-2 px-3 text-sm text-black" onKeyPress={handleKeyPress} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+            <input type="password" id="password" className="w-full  border-2 border-black rounded py-2 px-3 text-sm text-black" onKeyPress={handleKeyPress} value={password} onChange={handlePasswordChange} placeholder="Enter your password" />
             <Link href="/forgot" className="text-green-500 text-sm absolute top-0 right-0 mt-1 mr-1 font-bold  ">Forgot Password?</Link>
           </div>
 
