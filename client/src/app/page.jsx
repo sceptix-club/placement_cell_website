21b145636033b175f4609b1afe030e0f29bbdde9
“Pranaam19” <22j34.pranam@sjec.ac.in>
2023-12-29 20:20:43 +0530

    updated design

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,23 +1,22 @@
-
 import Drive from "@/components/drive_file";
 
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center items-center flex-col">
-        {/* Added flex-col */}
-        <section className="w-5/6">
-          {/* Increased width to 83.33% */}
-          <h2>Ongoing</h2>
+      <div className="flex justify-center items-center flex-col bg-#222222 h-screen">
+        {/* Added h-screen to make the container take full height */}
+        <section className="w-11/12">
+          {/* Increased width to 91.67% */}
+          <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>Ongoing</h2>
           <Drive></Drive>
         </section>
-        <section className="w-5/6">
-          {/* Increased width to 83.33% */}
-          <h2>Upcoming</h2>
+        {/* Add a line between the two sections */}
+        <hr className="my-4" />
+        <section className="w-11/12">
+          {/* Increased width to 91.67% */}
+          <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>Upcoming</h2>
           <Drive></Drive>
           {/* Add your ongoing section content here */}
-          
-          
         </section>
       </div>
     </>
