8d9e510290455614d2a6d62e68e84bf953e640ac
Swastikb794 <22g51.swastik@sjec.ac.in>
2023-12-30 02:06:53 +0530

    updated card color,corrected text size and color and added divider. Added card to the component.

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,23 +1,20 @@
-
-import Drive from "@/components/drive_file";
+import DriveCard from "@/components/driveCard";
+import DriveCard2 from "@/components/driveCard2";
 
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center items-center flex-col">
-        {/* Added flex-col */}
+      <div className="flex justify-center items-center flex-col bg-background-clr overflow-y-auto">
         <section className="w-5/6">
-          {/* Increased width to 83.33% */}
-          <h2>Ongoing</h2>
-          <Drive></Drive>
+          <h1 className="text-3xl font-bold text-white mb-4">Ongoing</h1>
+          <DriveCard />
+          <DriveCard />
         </section>
+        <hr className="w-5/6 border-white my-4" /> {/* Horizontal line */}
         <section className="w-5/6">
-          {/* Increased width to 83.33% */}
-          <h2>Upcoming</h2>
-          <Drive></Drive>
-          {/* Add your ongoing section content here */}
-          
-          
+          <h1 className="text-3xl font-bold text-white mb-4">Upcoming</h1>
+          <DriveCard2 />
+          <DriveCard2 />
         </section>
       </div>
     </>
