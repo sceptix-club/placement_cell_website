0b209d4f556e5a58a08462efcc1ebd078abe1f84
Swastikb794 <22g51.swastik@sjec.ac.in>
2023-12-29 15:51:07 +0530

    made card component

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,74 +1,23 @@
-import Image from "next/image";
+
+import Drive from "@/components/drive_file";
 
 const Home = () => {
-  const currentDate = new Date().toLocaleDateString();
-  const currentDateTime = new Date();
-
-  const calculateTimeDifference = (dateTime) => {
-    const now = new Date();
-    const diffInMinutes = Math.floor((now - dateTime) / (1000 * 60));
-    return `${diffInMinutes} mins ago`;
-  };
-
-  const cardData = [
-    {
-      title: "HP Card 1",
-      content: "Content for HP Card 1",
-    },
-    {
-      title: "HP Card 2",
-      content: "Content for HP Card 2",
-    },
-  ];
-
   return (
     <>
       <div className="flex justify-center items-center flex-col">
-        {" "}
         {/* Added flex-col */}
         <section className="w-5/6">
-          {" "}
           {/* Increased width to 83.33% */}
           <h2>Ongoing</h2>
-          {/* Add your ongoing section content here */}
-          <div className="flex justify-center">
-            <div className="m-4 flex flex-col border border-gray-300 bg-gray-800 rounded-lg p-4 w-full sm:w-screen h-48">
-              {" "}
-              {/* Changed width to full */}
-              <h3 className="text-lg font-bold">hp Card 1</h3>
-              <p>Content for hp Card 1</p>
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <div className="m-4 flex flex-col border border-gray-300 bg-gray-800 rounded-lg p-4 w-full sm:w-screen h-48">
-              {" "}
-              {/* Changed width to full */}
-              <h3 className="text-lg font-bold">hp Card 1</h3>
-              <p>Content for hp Card 1</p>
-            </div>
-          </div>
+          <Drive></Drive>
         </section>
         <section className="w-5/6">
-          {" "}
           {/* Increased width to 83.33% */}
           <h2>Upcoming</h2>
+          <Drive></Drive>
           {/* Add your ongoing section content here */}
-          <div className="flex justify-center">
-            <div className="m-4 flex flex-col border border-gray-300 bg-gray-800 rounded-lg p-4 w-full sm:w-screen h-48">
-              {" "}
-              {/* Changed width to full */}
-              <h3 className="text-lg font-bold">hp Card 1</h3>
-              <p>Content for hp Card 1</p>
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <div className="m-4 flex flex-col border border-gray-300 bg-gray-800 rounded-lg p-4 w-full sm:w-screen h-48">
-              {" "}
-              {/* Changed width to full */}
-              <h3 className="text-lg font-bold">hp Card 1</h3>
-              <p>Content for hp Card 1</p>
-            </div>
-          </div>
+          
+          
         </section>
       </div>
     </>
---
+++ client/src/components/drive_file.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const DriveFile = () => {
+  return (
+    
+          <div className="m-4 flex flex-col border border-gray-300 bg-gray-700 rounded-lg p-4 w-full sm:w-screen h-48">
+            <h3 className="text-lg font-bold">hp Card 1</h3>
+            <p>Content for hp Card 1</p>
+            <div className="mt-auto flex flex-col items-end">
+              <p className="text-xs text-green-500">4 mins ago</p>
+              <p className="text-xs text-white">28/12/2023</p>
+              <div className="flex space-x-2 mt-2">
+                <button className="bg-gray-700 text-white px-2 py-1 rounded">
+                  Data Analyst
+                </button>
+                <button className="bg-gray-700 text-white px-2 py-1 rounded">
+                  Roles
+                </button>
+                <button className="bg-gray-700 text-white px-2 py-1 rounded">
+                  Roles
+                </button>
+              </div>
+            </div>
+          </div>
+       
+  );
+};
+
+export default DriveFile;
