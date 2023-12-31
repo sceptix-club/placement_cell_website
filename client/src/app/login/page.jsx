b0edf4a39aed0d58f13735064f1df6a1011ba633
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2023-12-31 19:56:22 +0530

    made responsive

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -52,7 +52,7 @@ const login = () => {
 
 
       <div className="flex items-center justify-center h-screen font-sans bg-[#222222] ">
-        <div className="bg-white p-10 rounded shadow-md w-96 md:max-w-md lg:max-w-lg xl:max-w-xl hover-shadow">
+        <div className="bg-white p-10 rounded shadow-md w-96 hover-shadow md:max-w-md lg:max-w-lg xl:max-w-xl">
           <h2 className="text-2xl font-semibold mb-6  text-black text-center">Login to your account</h2>
 
 
