c34f3227772a58a80bffc05c5ee6e513dc9c5257
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-01-01 13:58:23 +0530

    added link

--- client/src/app/forgot/page.jsx
+++ client/src/app/forgot/page.jsx
@@ -1,5 +1,7 @@
 'use client'
 import React, { useState } from 'react';
+import Link from 'next/link';
+
 
 const Forgot = () => {
     const [email, setEmail] = useState('');
@@ -97,7 +99,9 @@ const Forgot = () => {
 
 
                     >
+
                         {otpSent ? 'Verify OTP' : 'Send OTP'}
+
                     </button>
 
                 )}
