e925db6f43f8f6119374bc02cf9142055e0b65da
Swastikb794 <22g51.swastik@sjec.ac.in>
2024-01-01 21:21:06 +0530

    Added margin to last section for proper spacing with the footer

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -8,18 +8,21 @@ const Home = () => {
     <>
       <div className="flex justify-center items-center flex-col bg-background-clr overflow-y-auto">
         <section className="w-2/3">
-          <h1 className="text-3xl font-bold text-white mb-6">Ongoingsss</h1>
-          {Data.map((placement) => (
+          <h1 className="text-3xl font-bold text-white mb-6">Ongoing</h1>
+          {Data.map((placement, index) => (
             <DriveCard key={placement.id} placement={placement} />
           ))}
         </section>
         <hr className="w-2/3 border-white my-6" />
         <section className="w-2/3">
           <h1 className="text-3xl font-bold text-white mb-6">Upcoming</h1>
-          {Data.map((placement) => (
+          {Data.map((placement, index) => (
             <DriveCard key={placement.id} placement={placement} />
           ))}
         </section>
+        <div className="w-2/3 mb-6">
+          <h1 className="text-3xl font-bold text-white mb-4"></h1>
+        </div>
       </div>
     </>
   );
