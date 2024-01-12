4204a299860c56f31cad65ccad2148df5c3a45ce
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-01-12 18:58:15 +0530

    added name of the role component

--- client/src/app/Role/page.jsx
+++ client/src/app/Role/page.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div>
-            <h1>Roles</h1>
+
+        <div className="p-8 border rounded-md shadow-md max-w-md mx-auto">
+            <div className="mb-4">
+                <label className="block text-sm font-semibold text-gray-600 mb-2">Name of the Role</label>
+                <input
+                    type="text"
+                    name="roleName"
+                    // value={jobInfo.roleName}
+                    // onChange={handleChange}
+                    className="w-full border rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+                />
+            </div>
         </div>
     );
 };
