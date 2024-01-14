38ca402fa1f89d25a2a5447572be7dc47df93b0e
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-01-14 18:28:41 +0530

    width and font changes

--- client/src/app/Role/page.jsx
+++ client/src/app/Role/page.jsx
@@ -25,97 +25,100 @@ const Role = () => {
     return (
 
         <div className="w-1/2 px-10 mx-auto">
-            <div className="p-5 border rounded-md bg-primary-card shadow-md">
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold text-main-heading mb-2">Name of the Role</label>
+            <h1 className="font-sans font-bold text-4xl">Create a Role</h1>
+            <div className="p-5 border rounded-md mt-5 mb-16  bg-primary-card shadow-md">
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold mt-5  text-main-heading mb-2">Name of the Role</label>
                     <input
                         type="text"
                         name="roleName"
                         value={jobInfo.roleName}
                         onChange={handleChange}
 
-                        className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                        className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                         placeholder="Enter the role..."
                     />
                 </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold  text-main-heading mb-2">Description</label>
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold  text-main-heading mb-2">Description</label>
                     <input
                         type="text"
                         name="description"
                         value={jobInfo.description}
                         onChange={handleChange}
-                        className="mt-1 p-10 w-full border border bg-secondary-card placeholder-plcholder-text text-divider-color rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                        className="mt-1 p-10 w-11/12 border  bg-secondary-card placeholder-plcholder-text text-divider-color rounded-md focus:outline-none focus:ring focus:border-blue-300"
                     ></input>
                 </div>
 
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold text-main-heading mb-2">Qualification</label>
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold text-main-heading mb-2">Qualification</label>
                     <input
                         type="text"
                         name="qualificationCutoff"
                         value={jobInfo.qualificationCutoff}
                         onChange={handleChange}
-                        className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                        className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                     />
                 </div>
                 <div className="flex space-x-8">
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold text-main-heading mb-2">Cutoff</label>
+                    <div className="mb-4 px-5">
+                        <label className="block text-lg font-semibold text-main-heading mb-2">Cutoff</label>
                         <input
                             type="text"
                             name="Cutoff"
                             value={jobInfo.Cutoff}
                             onChange={handleChange}
-                            className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                            className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                         />
                     </div>
 
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold text-main-heading mb-2">CTC</label>
+                    <div className="mb-4 px-5">
+                        <label className="block text-lg font-semibold text-main-heading mb-2">CTC</label>
                         <input
                             type="text"
                             name="ctc"
                             value={jobInfo.ctc}
                             onChange={handleChange}
-                            className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                            className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                         />
                     </div>
                 </div>
 
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold text-main-heading mb-2">Stipend</label>
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold text-main-heading mb-2">Stipend</label>
                     <input
                         type="text"
                         name="stipend"
                         value={jobInfo.stipend}
                         onChange={handleChange}
-                        className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                        className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                     />
                 </div>
 
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold text-main-heading mb-2">Location</label>
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold text-main-heading mb-2">Location</label>
                     <input
                         type="text"
                         name="location"
                         value={jobInfo.location}
                         onChange={handleChange}
-                        className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                        className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                     />
                 </div>
 
-                <div className="mb-4">
-                    <label className="block text-sm font-semibold text-main-heading mb-2">Service Agreement</label>
+                <div className="mb-4 px-5">
+                    <label className="block text-lg font-semibold text-main-heading mb-2">Service Agreement</label>
                     <input
                         type="text"
                         name="serviceAgreement"
                         value={jobInfo.serviceAgreement}
                         onChange={handleChange}
-                        className="w-full border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
+                        className="w-11/12 border rounded-md px-3 py-2 bg-secondary-card placeholder-plcholder-text text-divider-color focus:outline-none focus:border-blue-500"
                     />
                 </div>
-                <button>Save</button>
+                <button className="px-10 py-2 ml-5 mb-5 mt-5 bg-green-600 hover:bg-green-700 text-white font-bold  rounded-md">
+                    Save
+                </button>
             </div>
         </div>
     );
