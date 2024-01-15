73d0fb91168d063f5226f867c5209021050fef17
pranavvraja <pranavvraja@gmail.com>
2024-01-15 13:01:32 +0530

    port number to 3001 in server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -3,22 +3,23 @@ const app = express();
 import "dotenv/config";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser())
-app.use(cors())
-let PORT = process.env.PORT || 3000;
+app.use(cookieParser());
+app.use(cors());
+let PORT = process.env.PORT || 3001;
 import placementUpdate from "./routes/palcement_routes.mjs";
 import getdrives from "./routes/getdrives.mjs";
-import login from './routes/login.mjs'
+import login from "./routes/login.mjs";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 app.use("/api/placement", placementUpdate);
 app.use("/api/drives", getdrives);
-app.use("/api/login",login)
-
+app.use("/api/login", login);
 
 app.listen(PORT, () => {
-    console.log(`Server is running in port ${PORT}`);
+  console.log(`Server is running in port ${PORT}`);
 });
