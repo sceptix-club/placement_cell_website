7b56d9636a998149daa0d57a67e295daa4c4cd39
vyshnav <vyshnavravi599@gmail.com>
2024-01-17 22:19:31 +0530

    Fix import formatting in studentdetails.mjs

--- server/routes/studentdetails.mjs
+++ server/routes/studentdetails.mjs
@@ -4,9 +4,9 @@ import "dotenv/config";
 import bodyParser from "body-parser";
 app.use(bodyParser.json());
 const router = express.Router();
-import { getstudent } from '../controller/controller.mjs'
-import {verifyStudentJwtToken} from '../middleware/verify_jwt_token.mjs'
+import { getstudent } from "../controller/controller.mjs";
+import { verifyStudentJwtToken } from "../middleware/verify_jwt_token.mjs";
 
-router.get('/:id',verifyStudentJwtToken, getstudent);
+router.get("/:id", verifyStudentJwtToken, getstudent);
 
-export default router
+export default router;
