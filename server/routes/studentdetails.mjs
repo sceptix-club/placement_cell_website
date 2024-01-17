3caba60a56e4e33551c940ae5381e80594255162
vyshnav <vyshnavravi599@gmail.com>
2024-01-17 21:18:30 +0530

    Add JWT token verification to student details route

--- server/routes/studentdetails.mjs
+++ server/routes/studentdetails.mjs
@@ -5,7 +5,8 @@ import bodyParser from "body-parser";
 app.use(bodyParser.json());
 const router = express.Router();
 import { getstudent } from '../controller/controller.mjs'
+import {verifyStudentJwtToken} from '../middleware/verify_jwt_token.mjs'
 
-router.get('/:id', getstudent);
+router.get('/:id',verifyStudentJwtToken, getstudent);
 
 export default router
