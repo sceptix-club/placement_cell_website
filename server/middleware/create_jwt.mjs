2b040705877404a78a8ffeb12b1c382c5e96a971
vyshnav <vyshnavravi599@gmail.com>
2024-01-17 22:18:08 +0530

    Refactor createJwt middleware functions

--- server/middleware/create_jwt.mjs
+++ server/middleware/create_jwt.mjs
@@ -1,6 +1,6 @@
 import bodyParse from "body-parser";
 import Jwt from "jsonwebtoken";
-export const createJwtForManagers = (req,res,next) => {
+export const createJwtForManagers = (req, res, next) => {
     let jwt_token = Jwt.sign(process.env.MANAGER_SECRET_KEY, process.env.MANAGER_SECRET_KEY);
 
     res.setHeader("Set-Cookie", `jwt_token=${jwt_token}; Path=/; HttpOnly`);
@@ -8,21 +8,19 @@ export const createJwtForManagers = (req,res,next) => {
     return jwt_token;
 };
 
-export const createJwtForMentors = (req,res,next) => {
-    let jwt_token = Jwt.sign(process.env.MENTOR_SECRET_KEY, process.env.MENTOR_SECRET_KEY, );
+export const createJwtForMentors = (req, res, next) => {
+    let jwt_token = Jwt.sign(process.env.MENTOR_SECRET_KEY, process.env.MENTOR_SECRET_KEY);
 
     res.setHeader("Set-Cookie", `jwt_token=${jwt_token}; Path=/; HttpOnly`);
     res.status(200).send({ token: jwt_token });
     return jwt_token;
-    
-}
+};
 
 const studentDetails = {
-    name:"hppy",
+    name: "hppy",
     email: "happy@gmail.cm",
-    usn:"4so22"
-
-}
+    usn: "4so22",
+};
 
 export const createJwtForStudents = async (req, res, next) => {
     let jwt_token = await Jwt.sign(studentDetails, process.env.STUDENT_SECRET_KEY);
@@ -30,4 +28,4 @@ export const createJwtForStudents = async (req, res, next) => {
     req.jwt_token = jwt_token; // Store jwt_token in the request object
 
     next();
-}
+};
