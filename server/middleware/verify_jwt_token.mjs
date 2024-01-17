58b96b8d74954fa8b5ee876a8632f2172a253fdf
vyshnav <vyshnavravi599@gmail.com>
2024-01-17 22:19:25 +0530

    Refactor JWT token verification middleware

--- server/middleware/verify_jwt_token.mjs
+++ server/middleware/verify_jwt_token.mjs
@@ -1,92 +1,66 @@
-import  Jwt  from "jsonwebtoken";
-
+import Jwt from "jsonwebtoken";
 
 export const verifyManagerJwtToken = (req, res, next) => {
     let authHeader = req.headers.authorization;
     if (authHeader == undefined) {
-        res.status(401).send({error:"no token provided"})
-    }
-    else {
-        
-        let token = authHeader && authHeader.split(" ")[1]
+        res.status(401).send({ error: "no token provided" });
+    } else {
+        let token = authHeader && authHeader.split(" ")[1];
         Jwt.verify(token, process.env.MANAGER_SECRET_KEY, (err, decoded) => {
             if (err) {
-                res.status(401).send({ error: "Invalid token" })
-                return false
+                res.status(401).send({ error: "Invalid token" });
+                return false;
+            } else {
+                next();
+                return true;
             }
-            else { 
-                next()
-                return true
-            }
-            
-        })
-        
+        });
     }
-}
+};
 
-export const verifyMentorJwtToken = (req,res,next) => {
+export const verifyMentorJwtToken = (req, res, next) => {
     let authHeader = req.headers.authorization;
     if (authHeader == undefined) {
-        res.status(401).send({error:"no token provided"})
-    }
-    else {
-        
-        let token = authHeader && authHeader.split(" ")[1]
+        res.status(401).send({ error: "no token provided" });
+    } else {
+        let token = authHeader && authHeader.split(" ")[1];
         Jwt.verify(token, process.env.MENTOR_SECRET_KEY, (err, decoded) => {
             if (err) {
-                res.status(401).send({ error: "Invalid token" })
-                return false
+                res.status(401).send({ error: "Invalid token" });
+                return false;
+            } else {
+                console.log("decoded value is ", decoded);
+                next();
+                return true;
             }
-            else { 
-                console.log("decoded value is " ,decoded)
-                next()
-                return true
-            }
-            
-        })
-        
+        });
     }
-    
-}
+};
 
 export const verifyStudentJwtToken = (req, res, next) => {
     let authHeader = req.headers.authorization;
     if (authHeader == undefined) {
-        res.status(401).send({error:"no token provided"})
-    }
-    else {
-        
-        let token = authHeader && authHeader.split(" ")[1]
+        res.status(401).send({ error: "no token provided" });
+    } else {
+        let token = authHeader && authHeader.split(" ")[1];
         Jwt.verify(token, process.env.STUDENT_SECRET_KEY, (err, decoded) => {
             if (err) {
-                res.status(401).send({ error: "Invalid token" })
-                return false
-            }
-            else { 
-                let Paramusn  = req.params['id']
+                res.status(401).send({ error: "Invalid token" });
+                return false;
+            } else {
+                let Paramusn = req.params["id"];
                 // console.log("decoded value is " ,decoded)
-                if (Paramusn == decoded.usn && decoded.usn  == req.headers.usn) {
-                    req.isPageSame = true
+                if (Paramusn == decoded.usn && decoded.usn == req.headers.usn) {
+                    req.isPageSame = true;
                     // res.status(200).json({isPageSame:true})
-                    next()
-                    return true
-                }
-                else {
-                    req.isPageSame = false
-                   
-                    res.status(401).json({isPageSame : false})
+                    next();
+                    return true;
+                } else {
+                    req.isPageSame = false;
+
+                    res.status(401).json({ isPageSame: false });
                 }
             }
-            
-        })
-        
+        });
     }
-    
-}
-
-
-    
-
-
-
-
+};
