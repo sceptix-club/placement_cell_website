5de565b74f26362c8e7770dd9c0020b66468eb91
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-01-20 13:51:47 +0530

    made responsive

--- client/src/app/Role/page.jsx
+++ client/src/app/Role/page.jsx
@@ -33,26 +33,29 @@ const Role = () => {
 
     return (
 
-        <div className="w-1/2 px-10 mx-auto">
-            <h1 className="font-gabarito font-bold  text-4xl text-divider-color">Create a Role</h1>
-            <div className="p-5  rounded-md mt-5 mb-16  justify-center font-gabarito  bg-primary-card  ">
-                <div className="mb-4 px-5">
-                    <label className="block text-lg font-semibold mt-5  text-divider-color mb-2">Name of the Role</label>
+        <div className="flex flex-col justify-center ml-46  h-auto py-10 mb-10">
+            <h2 className="font-gabarito ml-10   lg:ml-96 sm:ml-20 text-2xl sm:text-3xl md:text-4xl   font-bold  text-divider-color mb-2">
+                Create a Role
+            </h2>
+
+            <section className="font-gabarito w-10/12 ml-10 md:ml-40 lg:ml-96 sm:text-2xl md:text-2xl  sm:ml-20    mt-4 p-4 sm:w-sm md:w-md lg:w-lg h-auto md:p-12 sm:p-8  bg-primary-card rounded-md">
+                <form className=" flex flex-col mb-4  ">
+                    <label className="block text-lg font-semibold mt-1 sm:text-xl md:text-2xl   text-divider-color mb-2">Name of the Role</label>
                     <input
                         type="text"
                         name="roleName"
                         value={jobInfo.roleName}
                         onChange={handleChange}
 
-                        className="w-11/12 rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color "
+                        className="  rounded-md px-3 py-3 md:px-3 md:py-3    text-sm bg-secondary-card placeholder-plcholder-text text-divider-color "
                         placeholder="Enter the role"
                     />
-                </div>
-                <div className="mb-4 px-5 flex-wrap h-auto">
 
-                    <label className="block text-lg font-semibold  text-divider-color mb-2">Description</label>
+
+
+                    <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl   text-divider-color mb-2">Description</label>
                     <textarea
-                        className="w-11/12 pb-10 pt-1  px-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color rounded-md resize-none overflow-hidden "
+                        className="pb-10 pt-1  px-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color rounded-md resize-none overflow-hidden "
 
                         name="description"
 
@@ -65,86 +68,92 @@ const Role = () => {
 
                         placeholder="Enter the description..."
                     />
-                </div>
 
-                <div className="mb-4 px-5">
-                    <label className="block text-lg font-semibold text-divider-color mb-2">Qualification</label>
+
+
+                    <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Qualification</label>
                     <input
                         type="text"
                         name="qualificationCutoff"
                         value={jobInfo.qualificationCutoff}
                         onChange={handleChange}
-                        className="w-11/12  rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
+                        className=" rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Qualification"
                     />
-                </div>
 
-                <div className="mb-4 px-5  ">
-                    <label className="block text-lg font-semibold text-divider-color mb-2">Cutoff</label>
+
+
+                    <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Cutoff</label>
                     <input
                         type="text"
                         name="Cutoff"
                         value={jobInfo.Cutoff}
                         onChange={handleChange}
-                        className="w-11/12   rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
+                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Cutoff"
                     />
-                </div>
-                <div className="flex ">
 
-                    <div className="mb-4 px-5">
-                        <label className="block text-lg font-semibold text-divider-color mb-2">CTC</label>
-                        <input
-                            type="text"
-                            name="ctc"
-                            value={jobInfo.ctc}
-                            onChange={handleChange}
-                            className=" w-56 rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
-                            placeholder="Enter the CTC"
-                        />
+
+
+                    <div className="flex flex-wrap">
+                        <div className="flex-1 pr-2">
+                            <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">CTC</label>
+                            <input
+                                type="text"
+                                name="ctc"
+                                value={jobInfo.ctc}
+                                onChange={handleChange}
+                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color"
+                                placeholder="Enter the CTC"
+                            />
+                        </div>
+
+                        <div className="flex-1 pl-2">
+                            <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Stipend</label>
+                            <input
+                                type="text"
+                                name="stipend"
+                                value={jobInfo.stipend}
+                                onChange={handleChange}
+                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color"
+                                placeholder="Enter the Stipend"
+                            />
+                        </div>
                     </div>
 
 
-                    <div className="mb-4 ml-14 px-5">
-                        <label className="block text-lg font-semibold text-divider-color mb-2">Stipend</label>
-                        <input
-                            type="text"
-                            name="stipend"
-                            value={jobInfo.stipend}
-                            onChange={handleChange}
-                            className="w-56 rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
-                            placeholder="Enter the Stipend"
-                        />
-                    </div>
-                </div>
-
-                <div className="mb-4 px-5">
-                    <label className="block text-lg font-semibold text-divider-color mb-2">Location</label>
+                    <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Location</label>
                     <input
                         type="text"
                         name="location"
                         value={jobInfo.location}
                         onChange={handleChange}
-                        className="w-11/12  rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
+                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Location"
                     />
-                </div>
 
-                <div className="mb-4 px-5">
-                    <label className="block text-lg font-semibold text-divider-color mb-2">Service Agreement</label>
+
+
+                    <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Service Agreement</label>
                     <input
                         type="text"
                         name="serviceAgreement"
                         value={jobInfo.serviceAgreement}
                         onChange={handleChange}
-                        className="w-11/12 rounded-md px-3 py-2 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
+                        className="rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Service Agreement"
                     />
-                </div>
-                <button className="px-10 py-2 ml-5 mb-5 mt-5 bg-green-600 hover:bg-green-700 text-white font-bold  rounded-md">
-                    Save
-                </button>
-            </div>
+                    <div className="flex justify-center w-32 h-10">
+
+                        <button
+                            className="font-medium bg-logo-bg w-32 h-10 rounded-md mt-5"
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </section>
         </div >
     );
 };
