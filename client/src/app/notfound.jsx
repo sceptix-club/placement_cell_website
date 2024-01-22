5f7668a505a5e6b8b59bb3bb25120c2b8a73a6de
Shivani-rao123 <22c19.shivanib@sjec.ac.in>
2024-01-22 21:07:43 +0530

    removed the scrolling and corrected the animation

--- client/src/app/notfound.jsx
+++ client/src/app/notfound.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-background-clr text-primary-text relative">
+    <div className="flex flex-col items-center justify-center bg-background-clr text-primary-text relative">
       <div className="h-1/3 flex flex-col items-center justify-center">
         <div className="text-3xl text-white text-center font-bold mb-2 hover:text-white hover:shadow-md transition-all duration-300">
           WHOOPS
@@ -12,7 +12,7 @@ const NotFound = () => {
       </div>
 
       <div className="h-1/3 flex items-center justify-center relative">
-        <div className="relative z-10 text-[36vh] text-logo-bg text-center font-bold hover:bottom-[20px] hover:right-[5px] transform hover:scale-110 transition-transform duration-300">
+        <div className="relative z-10 text-[36vh] text-logo-bg text-center font-bold transform hover:translate-y-[-15px] hover:translate-x-[-10px] transition-transform duration-900">
           404
         </div>
         <div className="absolute text-[36vh] text-white text-center font-bold">
