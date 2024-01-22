75a89a875adea51f116907a372a8ea52114b35e1
Shivani-rao123 <22c19.shivanib@sjec.ac.in>
2024-01-22 21:49:23 +0530

    same changes for 500 not found as that of 404

--- client/src/components/500notfound.jsx
+++ client/src/components/500notfound.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Link from "next/link";
 
-const ServerError = () => {
+const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-background-clr text-primary-text relative">
+    <div className="flex flex-col items-center justify-center bg-background-clr text-primary-text relative">
       <div className="h-1/3 flex flex-col items-center justify-center">
         <div className="text-3xl text-white text-center font-bold mb-2 hover:text-white hover:shadow-md transition-all duration-300">
           WHOOPS
@@ -12,8 +12,8 @@ const ServerError = () => {
       </div>
 
       <div className="h-1/3 flex items-center justify-center relative">
-        <div className="relative z-10 text-[36vh] text-logo-bg text-center font-bold hover:bottom-[20px] hover:right-[5px] transform hover:scale-110 transition-transform duration-300">
-         500
+        <div className="relative z-10 text-[36vh] text-logo-bg text-center font-bold transform hover:translate-y-[-15px] hover:translate-x-[-10px] transition-transform duration-900">
+          500
         </div>
         <div className="absolute text-[36vh] text-white text-center font-bold">
           500
@@ -21,7 +21,7 @@ const ServerError = () => {
       </div>
 
       <div className="h-1/3 flex flex-col items-center justify-center mt-0">
-        <div className="text-[30px] text-white mb-2">Internal Server Error</div>
+        <div className="text-[30px] text-white mb-2">Internal server error!</div>
         <div className="text-lg text-white mb-2">
           Return to&nbsp;
           <Link href="./" className="text-green-500 hover:underline">
@@ -33,4 +33,4 @@ const ServerError = () => {
   );
 };
 
-export default ServerError;
+export default NotFound;
