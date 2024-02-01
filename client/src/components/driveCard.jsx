095a5718717e306a898d1c3baf66e7197b6b73e1
Swastikb794 <22g51.swastik@sjec.ac.in>
2024-02-01 22:33:14 +0530

    made the main page responsive

--- client/src/components/driveCard.jsx
+++ client/src/components/driveCard.jsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 const DriveCard = ({ placement }) => {
   return (
     <Link href={`/drive/${placement.id}`} passHref>
-      <div className="m-4 p-6 mb-6 flex flex-row bg-primary-card rounded-lg w-full cursor-pointer">
-        <div className="flex flex-col w-1/2">
+      <div className="m-4 p-6 mb-6 flex flex-col md:flex-row bg-primary-card rounded-lg w-full cursor-pointer">
+        <div className="md:w-1/2">
           <h3 className="text-2xl font-bold mb-4">{placement.placementName}</h3>
           <p className="text-xl font-bold mb-4">{placement.companyName}</p>
         </div>
-        <div className="flex flex-col items-end w-1/2">
-          <div className="flex justify-end mb-4">
-            <p className="text-xl text-white">{placement.date}</p>
+        <div className="md:w-1/2 md:text-right">
+          <div className="lg:md:flex justify-end mb-4">
+            <p className="text-xl text-white ">{placement.date}</p>
           </div>
-          <div className=" text-balance flex space-x-4  px-4 py-2 align-text-button sm:flex space-x-2 px-2 py-2 text-balance align-text-button ">
+          <div className="lg:md:flex flex-wrap justify-end  ">
             {placement.roles.map((role, index) => (
               <button
                 key={index}
-                className="bg-secondary-card text-balance text-white text-sm  rounded sm:bg-secondary-card text-white  px-1 py-1 rounded"
+                className="bg-secondary-card text-white text-sm px-4 py-2 mb-2 rounded mr-2"
               >
                 {role.role}
               </button>
