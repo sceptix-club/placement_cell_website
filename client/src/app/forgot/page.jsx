8ce66cd88b4877d2d1224840ab115d15f57a95b4
22c60.vyshnav <22c60.vyshnav@sjec.ac.in>
2024-03-09 08:48:00 +0000

    refactor forgot password component

--- client/src/app/forgot/page.jsx
+++ client/src/app/forgot/page.jsx
@@ -4,13 +4,15 @@ import Link from 'next/link';
 import supabase from '@/data/supabase';
 
 
+
 const Forgot = () => {
+  
     const [email, setEmail] = useState('');
     const [otpSent, setOtpSent] = useState(false);
     const [otp, setOtp] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const emailRegex = /@sjec\.ac\.in/;
-    const handleSendOtp = async() => {
+    const handleSendEmail = async() => {
         if (email.trim() === '') {
             setErrorMessage('Invalid Credentials: Email cannot be empty or incorrect.');
             return;
@@ -20,32 +22,20 @@ const Forgot = () => {
             return;
         }
         const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: 'https://example.com/update-password',
+            redirectTo: `${window.location.href}`
         })
         if (data) {
             console.log(data, error)
         }
 
         setErrorMessage('check your email id');
-        // setOtpSent(true);
-        console.log(`OTP Sent to ${email}`);
-    };
 
-    const handleVerifyOtp = () => {
-        if (otp.trim() === '' || !/^\d{6}$/.test(otp)) {
-            setErrorMessage('Invalid OTP: Please enter a valid 6-digit OTP.');
-            return;
-        }
-
-        setErrorMessage('');
-        console.log(`Verified OTP: ${otp}`);
     };
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            otpSent ? handleVerifyOtp() : handleSendOtp();
-        }
-    };
+    const handleKeyPress = (e)=>{
+        if(e.key === 'Enter'){
+            handleSendEmail();
+    }
+}
 
     return (
         <div className="flex items-center justify-center font-sans h-screen  bg-[#222222]">
@@ -73,45 +63,11 @@ const Forgot = () => {
                     type="button"
                     className={`bg-green-700 text-white py-2 px-3 w-full justify-center rounded  mb-4 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 ${otpSent ? 'cursor-not-allowed opacity-50' : ''
                         }`}
-                    onClick={handleSendOtp}
-                    disabled={otpSent}
+                    onClick={handleSendEmail}
+                
                 >
-                    {otpSent ? 'OTP Sent' : 'Send OTP'}
+                    Send Email
                 </button>
-
-                {otpSent && (
-                    <div className="flex mb-4">
-                        <div className="mr-2">
-                            <label htmlFor="otp" className="block text-gray-600 text-sm font-bold mb-2">Enter 6-Digit OTP Sent To Your Email</label>
-                            <input
-                                type="text"
-                                id="otp"
-                                className=" w-32 border-2 border-gray-300 rounded py-2 px-3 mt-2 text-sm text-black"
-                                value={otp}
-                                onKeyPress={handleKeyPress}
-                                onChange={(e) => setOtp(e.target.value)}
-
-                            />
-                        </div>
-
-
-
-
-                        <button
-                            type="button"
-                            className=" bg-green-700 text-white py-2 px-2 rounded w-96  mb-4 mt-14 ml-2 mr-20  content-center hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-                            onClick={otpSent ? handleVerifyOtp : handleSendOtp}
-
-
-
-                        >
-                            <label htmlFor="verify" className="py-1 text-sm">Verify OTP</label>
-
-
-                        </button>
-                    </div>
-
-                )}
 
                 {errorMessage && <p className="text-red-500 mb-5  text-center">{errorMessage}</p>}
             </div>
