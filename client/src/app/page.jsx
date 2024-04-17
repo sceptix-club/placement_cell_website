cefdff2e27abaef6c9245c3e876a89da11aa1b36
Pranavv Raja <pranavvraja@gmail.com>
2024-04-17 15:36:06 +0530

    fetch drive from db

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,24 +1,40 @@
 // page.jsx
+"use client";
 import React from "react";
 import DriveCard from "@/components/driveCard";
-import Data from "../../public/data";
+// import Data from "../../public/data";
+import supabase from "@/data/supabase";
 
 const Home = () => {
+  const [placements, setPlacements] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchPlacement = async () => {
+      const { data, error } = await supabase.from("drive").select();
+      if (!error) {
+        setPlacements(data);
+        console.log("data");
+        console.log(data);
+      }
+    };
+    fetchPlacement();
+  }, []);
+
   return (
     <>
       <div className="lg:flex w-full justify-center items-center flex-col bg-background-clr overflow-y-auto sm: flex l">
         <section className="lg:w-2/3 sm: w-3/4">
           <h1 className="text-3xl font-bold text-white mb-6">Ongoing</h1>
-          {Data.map((placement, index) => (
+          {placements.map((placement, index) => (
             <DriveCard key={placement.id} placement={placement} />
           ))}
         </section>
         <hr className="w-2/3 border-white my-6" />
         <section className="lg:w-2/3 sm: w-3/4">
           <h1 className="text-3xl font-bold text-white mb-6">Upcoming</h1>
-          {Data.map((placement, index) => (
+          {/* {Data.map((placement, index) => (
             <DriveCard key={placement.id} placement={placement} />
-          ))}
+          ))} */}
         </section>
         <div className="w-2/3 mb-6">
           <h1 className="text-3xl font-bold text-white mb-4"></h1>
--- client/src/components/driveCard.jsx
+++ client/src/components/driveCard.jsx
@@ -7,22 +7,22 @@ const DriveCard = ({ placement }) => {
     <Link href={`/drive/${placement.id}`} passHref>
       <div className="m-4 p-6 mb-6 flex flex-col md:flex-row bg-primary-card rounded-lg w-full cursor-pointer">
         <div className="md:w-1/2">
-          <h3 className="text-2xl font-bold mb-4">{placement.placementName}</h3>
-          <p className="text-xl font-bold mb-4">{placement.companyName}</p>
+          <h3 className="text-2xl font-bold mb-4">{placement.name}</h3>
+          <p className="text-xl font-bold mb-4">{placement.company}</p>
         </div>
         <div className="md:w-1/2 md:text-right">
           <div className="lg:md:flex justify-end mb-4">
             <p className="text-xl text-white ">{placement.date}</p>
           </div>
           <div className="lg:md:flex flex-wrap justify-end  ">
-            {placement.roles.map((role, index) => (
+            {/* {placement.roles.map((role, index) => (
               <button
                 key={index}
                 className="bg-secondary-card text-white text-sm px-4 py-2 mb-2 rounded mr-2"
               >
                 {role.role}
               </button>
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
