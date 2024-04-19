29f957ad729cb2a62c361be80d8691cca0ec02f7
MacDsouza <120902232+MacDsouza@users.noreply.github.com>
2024-04-19 08:58:31 +0530

    Changed the button position

--- client/src/app/mentees/page.jsx
+++ client/src/app/mentees/page.jsx
@@ -4,7 +4,7 @@ import mentees from "../../../public/MenteeTestData.js"
 export default function page() {
   return (
     <div className="py-10" >
-      <p className="px-7 py-4 text-3xl font-medium">Your Mentees</p>
+      <p className="px-20 py-4 text-3xl font-medium">Your Mentees</p>
       <div className="flex flex-wrap justify-center mx-1 py-5 gap-4">
       {mentees.map((user, index) => (
         <div key={index} className="w-64 mt-3">
--- client/src/components/MenteeCard.jsx
+++ client/src/components/MenteeCard.jsx
@@ -1,15 +1,15 @@
 function MenteeCard({ profileImage, name, usn, branch, year, email }) {
   return (
     <>
-      <div className="w-64 space-y-1">
-        <div className="flex flex-col bg-primary-card rounded-md h-80 w-full p-4 items-center space-y-3">
+      <div className="w-64 bg-primary-card p-2 space-y-1 rounded-xl">
+        <div className="flex flex-col h-80 w-full p-4 items-center space-y-3">
           <img
             src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
             alt="ProfileImg"
             className="w-24 h-24 rounded-lg mb-2"
           />
           <div className="space-y-3">
-            <h3 className="text-xl mb-2 text-center">{name}</h3>
+            <h5 className="text-xl mb-2 text-center">{name}</h5>
             <p className="text-sm mb-1 text-left">USN: {usn}</p>
             <p className="text-sm mb-1 text-left">Branch: {branch}</p>
             <p className="text-sm mb-1 text-left">Year: {year}</p>
