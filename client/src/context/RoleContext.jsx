f096370a332e1c7f95308dbd0b8bacb212bf51e2
Pranavv Raja <pranavvraja@gmail.com>
2024-04-24 20:36:06 +0530

    implemented useContext for user roles

---
+++ client/src/context/RoleContext.jsx
@@ -0,0 +1,47 @@
+"use client";
+import Role from "@/app/create/role/[id]/page";
+import React, { createContext, useState, useEffect, useContext } from "react";
+import supabase from "@/data/supabase";
+
+const RoleContext = createContext();
+
+export const RoleProvider = ({ children }) => {
+  const [userRole, setUserRole] = useState(1);
+
+  let userID = 0;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data.session !== null) {
+        const { user } = data.session;
+        userID = user.id;
+        console.log("fetch_user3", userID);
+        fetchRole();
+      }
+    };
+
+    const fetchRole = async () => {
+      if (userID === 0) {
+        console.log("null");
+        return;
+      }
+      const { data, error } = await supabase
+        .from("user")
+        .select("role")
+        .eq("user_id", userID);
+      if (!error) {
+        const data1 = data[0].role;
+        setUserRole(data1);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <RoleContext.Provider value={{ userRole }}>{children}</RoleContext.Provider>
+  );
+};
+
+export const useRoleContext = () => useContext(RoleContext);
