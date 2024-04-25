b103903a087162087b9799653ce09d8370ca3cac
Pranavv Raja <pranavvraja@gmail.com>
2024-04-25 15:38:06 +0530

    to /drive/[id] added roleContext, modified Placement API, button to register button

--- client/src/app/drive/[id]/page.jsx
+++ client/src/app/drive/[id]/page.jsx
@@ -4,9 +4,10 @@ import RolesCard from "@/components/RolesCard";
 // import Data from "../../../../public/data";
 import { usePathname } from "next/navigation";
 import { notFound } from "next/navigation";
-import PlacementAPI   from '@/app/api/PlacementAPI';
-import Button from '@/components/Registerbutton';
-
+import PlacementAPI from "@/app/api/PlacementAPI";
+import RegisterButton from "@/components/Registerbutton";
+import { useRoleContext } from "@/context/RoleContext";
+// import ManagerDriveButtons from "@/components/ManagerDriveButtons";
 
 import supabase from "@/data/supabase";
 
@@ -14,16 +15,33 @@ const driveinfo = () => {
   const [placements, setPlacements] = useState([]);
   const [role, setRole] = useState([]);
 
+  const { userRole } = useRoleContext();
+  const [show, setShow] = useState(false);
+
   const pathName = usePathname();
   const pathNo = pathName.slice("/drive/".length);
   const [roleId, setRoleId] = useState(null); // Define roleId state
 
   // const dataAll = Data.find((item) => item.id === Number(pathNo));
 
+  useEffect(() => {
+    const checkUserRole = () => {
+      if (userRole === 3) {
+        setShow(true);
+      }
+    };
+    checkUserRole();
+  }, [userRole]);
+
   return (
     <div className="flex items-center justify-center py-10 mb-10 h-auto bg-background-clr font-inter font-normal">
       <section className="flex flex-col p-5 sm:p-8 lg:p-16  w-11/12 sm:w-10/12 md:w-2/3 lg:w-3/5 border-white h-auto rounded-md bg-primary-card">
-      <PlacementAPI pathNo={pathNo} setPlacements={setPlacements} setRole={setRole} />
+        <PlacementAPI
+          pathNo={pathNo}
+          setPlacements={setPlacements}
+          setRole={setRole}
+          setRoleIds={setRoleId}
+        />
         <h2 className="text-lg lg:text-2xl text-role-text font-semibold">
           {placements.name}
         </h2>
@@ -35,7 +53,7 @@ const driveinfo = () => {
         </div>
         <div className="flex flex-row item-center mt-4 lg:mt-5">
           <h3 className="text-sm lg:text-lg font-medium">Roles:&nbsp;</h3>
-          
+
           {role.map((role) => {
             return (
               <p
@@ -47,12 +65,13 @@ const driveinfo = () => {
             );
           })}
         </div>
-        <Button/>
+        <RegisterButton />
         <hr className=" border-divider-color mt-5" />
 
         {role.map((innerRole) => {
           return <RolesCard key={innerRole.id} props={innerRole} />;
         })}
+        {/* {show && <ManagerDriveButtons />} */}
       </section>
     </div>
   );
