3e1084c33ca1fe9ac863e1b36a35f1374ad13715
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-04-29 16:08:13 +0530

    Merge branch 'main' of https://github.com/Rishal14/placement_cell_website

--- client/src/app/create/role/page.jsx
+++ client/src/app/create/role/page.jsx
@@ -1,32 +1,18 @@
 'use client'
 import React, { useState, useRef, useEffect } from 'react';
-import supabase from '@/data/supabase';
-import { useRouter } from 'next/navigation'
-import { usePathname } from "next/navigation";
-
 
 
 
 const Role = () => {
-    const pathName = usePathname();
-    const pathNo = pathName.slice("/create/role/".length);
-    const [roleId, setRoleId] = useState(null);
-
-
-
-
     const [jobInfo, setJobInfo] = useState({
-        name: '',
+        roleName: '',
         description: '',
         qualification: '',
-        cutoff: '',
+        Cutoff: '',
         ctc: '',
         stipend: '',
         location: '',
         serviceAgreement: '',
-
-
-
     });
     const textAreaRef = useRef(null);
 
@@ -37,46 +23,8 @@ const Role = () => {
             [name]: value,
         });
     };
-    const saveRole = async () => {
-        try {
-
-            // console.log('Drive ID:', driveIdInt);
-
-
-
-
-            const { data, error } = await supabase
-                .schema("placements")
-                .from("role")
-                .insert([{ ...jobInfo, drive_id: pathNo }]);
-            console.log("hi");
-            if (error) {
-                console.error("Error saving role:", error);
-            } else {
-                console.log("Role saved successfully:", jobInfo);
-
-
-
-
-            }
-            setJobInfo({
-                name: '',
-                description: '',
-                qualification: '',
-                cutoff: '',
-                ctc: '',
-                stipend: '',
-                location: '',
-                serviceAgreement: '',
-
-            });
-        } catch (error) {
-            console.error("Error saving role:", error.message);
-        }
-    }
     useEffect(() => {
         textAreaRef.current.style.height = "auto";
-        console.log("path", pathNo);
 
         textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
     }, [jobInfo])
@@ -95,11 +43,11 @@ const Role = () => {
                     <label className="block text-lg font-semibold mt-1 sm:text-xl md:text-2xl   text-divider-color mb-2">Name of the Role</label>
                     <input
                         type="text"
-                        name="name"
-                        value={jobInfo.name}
+                        name="roleName"
+                        value={jobInfo.roleName}
                         onChange={handleChange}
 
-                        className="  rounded-md px-3 py-3 md:px-3 md:py-3    text-sm bg-secondary-card placeholder-plcholder-text text-white "
+                        className="  rounded-md px-3 py-3 md:px-3 md:py-3    text-sm bg-secondary-card placeholder-plcholder-text text-divider-color "
                         placeholder="Enter the role"
                     />
 
@@ -107,7 +55,7 @@ const Role = () => {
 
                     <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl   text-divider-color mb-2">Description</label>
                     <textarea
-                        className="pb-10 pt-1  px-3 text-sm bg-secondary-card placeholder-plcholder-text text-white rounded-md resize-none overflow-hidden "
+                        className="pb-10 pt-1  px-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color rounded-md resize-none overflow-hidden "
 
                         name="description"
 
@@ -126,10 +74,10 @@ const Role = () => {
                     <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Qualification</label>
                     <input
                         type="text"
-                        name="qualification"
-                        value={jobInfo.qualification}
+                        name="qualificationCutoff"
+                        value={jobInfo.qualificationCutoff}
                         onChange={handleChange}
-                        className=" rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white  "
+                        className=" rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Qualification"
                     />
 
@@ -138,10 +86,10 @@ const Role = () => {
                     <label className="block text-lg font-semibold mt-5 sm:text-xl md:text-2xl  text-divider-color mb-2">Cutoff</label>
                     <input
                         type="text"
-                        name="cutoff"
-                        value={jobInfo.cutoff}
+                        name="Cutoff"
+                        value={jobInfo.Cutoff}
                         onChange={handleChange}
-                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white  "
+                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Cutoff"
                     />
 
@@ -155,7 +103,7 @@ const Role = () => {
                                 name="ctc"
                                 value={jobInfo.ctc}
                                 onChange={handleChange}
-                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white"
+                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color"
                                 placeholder="Enter the CTC"
                             />
                         </div>
@@ -167,7 +115,7 @@ const Role = () => {
                                 name="stipend"
                                 value={jobInfo.stipend}
                                 onChange={handleChange}
-                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white"
+                                className="w-full mt-1 rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color"
                                 placeholder="Enter the Stipend"
                             />
                         </div>
@@ -180,7 +128,7 @@ const Role = () => {
                         name="location"
                         value={jobInfo.location}
                         onChange={handleChange}
-                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white  "
+                        className="  rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Location"
                     />
 
@@ -192,16 +140,14 @@ const Role = () => {
                         name="serviceAgreement"
                         value={jobInfo.serviceAgreement}
                         onChange={handleChange}
-                        className="rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-white  "
+                        className="rounded-md px-3 py-3 text-sm bg-secondary-card placeholder-plcholder-text text-divider-color  "
                         placeholder="Enter the Service Agreement"
                     />
                     <div className="flex justify-center w-32 h-10">
 
                         <button
-                            onClick={saveRole}
                             className="  text-lg bg-logo-bg w-32 h-10 rounded-md mt-5"
-                            type="button"
-
+                            type="submit"
                         >
                             Save
                         </button>
@@ -211,7 +157,5 @@ const Role = () => {
         </div >
     );
 };
-
-
 
 export default Role;
--- client/src/app/drive/[id]/page.jsx
+++ client/src/app/drive/[id]/page.jsx
@@ -34,95 +34,75 @@ const driveinfo = () => {
   // const dataAll = Data.find((item) => item.id === Number(pathNo));
 
   useEffect(() => {
-    const fetchPlacement = async () => {
-      const { data, error } = await supabase
-        .schema("placements")
-        .from("drive")
-        .select("*")
-        .eq("id", pathNo)
-        .single();
-      if (!error) {
-        setPlacements(data);
-        console.log("data");
-        console.log(data);
+    const checkUserRole = () => {
+      if (userRole === 3) {
+        setShow(true);
       }
     };
-
-    const fetchRoles = async () => {
-      const { data, error } = await supabase
-        .schema("placements")
-        .from("role")
-        .select("*")
-        .eq("drive_id", pathNo);
-      if (!error) {
-        setRole(data);
-        console.log("data1");
-        console.log(data);
-      }
-    };
-
-    fetchPlacement();
-    fetchRoles();
-  }, []);
+    checkUserRole();
+  }, [userRole]);
 
   // if (!dataAll) {
   //   return notFound();
   // }
 
-  router.push(`/create/role/${pathNo}`);
-};
-return (
-  <div className="flex items-center justify-center py-10 mb-10 h-auto bg-background-clr font-inter font-normal">
-    <section className="flex flex-col p-5 sm:p-8 lg:p-16  w-11/12 sm:w-10/12 md:w-2/3 lg:w-3/5 border-white h-auto rounded-md bg-primary-card">
-      <PlacementAPI
-        pathNo={pathNo}
-        setPlacements={setPlacements}
-        setRole={setRole}
-        setRoleIds={setRoleId}
-      />
-      <h2 className="text-lg lg:text-2xl text-role-text font-semibold">
-        {placements.name}
-      </h2>
-      <h2 className="text-2xl lg:text-4xl mb-1 font-semibold">
-        {placements.company}
-      </h2>
-      <div className="text-md lg:text-xl py-2 leading-tight lg:leading-tight font-medium">
-        <p>{placements.description}</p>
-      </div>
-      <div className="flex flex-row item-center mt-4 lg:mt-5">
-        <h3 className="text-sm lg:text-lg font-medium">Roles:&nbsp;</h3>
+  const handleAddRole = () => {
+    router.push(`/create/role/${pathNo}`);
+  };
+  return (
+    <div className="flex items-center justify-center py-10 mb-10 h-auto bg-background-clr font-inter font-normal">
+      <section className="flex flex-col p-5 sm:p-8 lg:p-16  w-11/12 sm:w-10/12 md:w-2/3 lg:w-3/5 border-white h-auto rounded-md bg-primary-card">
+        <PlacementAPI
+          pathNo={pathNo}
+          setPlacements={setPlacements}
+          setRole={setRole}
+          setRoleIds={setRoleId}
+        />
+        <h2 className="text-lg lg:text-2xl text-role-text font-semibold">
+          {placements.name}
+        </h2>
+        <h2 className="text-2xl lg:text-4xl mb-1 font-semibold">
+          {placements.company}
+        </h2>
+        <div className="text-md lg:text-xl py-2 leading-tight lg:leading-tight font-medium">
+          <p>{placements.description}</p>
+        </div>
+        <div className="flex flex-row item-center mt-4 lg:mt-5">
+          <h3 className="text-sm lg:text-lg font-medium">Roles:&nbsp;</h3>
 
-        {role.map((role) => {
-          return (
-            <p
-              key={role.id}
-              className="bg-secondary-card text-role-text-2 rounded-md px-2 ml-2 text-sm lg:text-lg font-medium"
-            >
-              {role.name}
-            </p>
+          {role.map((role) => {
+            return (
+              <p
+                key={role.id}
+                className="bg-secondary-card text-role-text-2 rounded-md px-2 ml-2 text-sm lg:text-lg font-medium"
+              >
+                {role.name}
+              </p>
 
-          );
+            );
+          })}
+
+        </div>
+        <RegisterButton />
+        <hr className=" border-divider-color mt-5" />
+        <button
+
+          onClick={handleAddRole}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mt-10 rounded"
+        >
+          Add Role
+        </button>
+
+
+        {role.map((innerRole) => {
+          return <RolesCard key={innerRole.id} props={innerRole} />;
         })}
+        {/* {show && <ManagerDriveButtons />} */}
+      </section>
 
-      </div>
-      <hr className=" border-divider-color mt-5" />
-      <button
-
-        onClick={handleAddRole}
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mt-10 rounded"
-      >
-        Add Role
-      </button>
-
-
-      {role.map((innerRole) => {
-        return <RolesCard key={innerRole.id} props={innerRole} />;
-      })}
-      {/* {show && <ManagerDriveButtons />} */}
-    </section>
-
-  </div>
-);
+    </div>
+  );
 };
+
 
 export default driveinfo;
