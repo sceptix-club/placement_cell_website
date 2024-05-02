f1e91c43af12326486742795e66df28868c5881d
Pranavv Raja <pranavvraja@gmail.com>
2024-05-02 21:06:29 +0530

    added managerDriveButtons to /drive/[id]

---
+++ client/src/components/ManagerDriveButtons.jsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import supabase from "@/data/supabase";
+import { useRouter } from "next/navigation";
+
+export default function ManagerDriveButtons(props) {
+  const pathName = usePathname();
+  const pathNo = pathName.slice("/drive/".length);
+
+  const router = useRouter();
+  const prop = props.props;
+  const [showPublish, setShowPublish] = React.useState(true);
+
+  console.log("prop", props);
+  console.log("props", props.props);
+  console.log("prop2", prop);
+  console.log("showPublish", showPublish);
+
+  const publishDrive = async () => {
+    const { data, error } = await supabase
+      .schema("placements")
+      .from("drive")
+      .update({ is_draft: false })
+      .eq("id", pathNo)
+      .select();
+    setShowPublish(false);
+    console.log("Drive Published");
+  };
+
+  const unPublishDrive = async () => {
+    const { data, error } = await supabase
+      .schema("placements")
+      .from("drive")
+      .update({ is_draft: true })
+      .eq("id", pathNo)
+      .select();
+    setShowPublish(true);
+    console.log("Drive Unpublished");
+  };
+
+  React.useEffect(() => {
+    const timeoutFunction = setTimeout(() => {
+      setShowPublish(prop);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-row p-2 justify-around">
+        <Link href={`/create/role/${pathNo}`}>
+          <button className="bg-logo-bg text-black font-bold  p-2  rounded-md m-3 text-sm">
+            Add Role
+          </button>
+        </Link>
+
+        <button className="bg-logo-bg text-black font-bold  p-2 rounded-md m-3 text-sm">
+          Edit Drive
+        </button>
+
+        {/* {showPublish ? ( */}
+        {/* // This is the condition that needs to be checked, not working, to be fixed soon */}
+        <button
+          className="bg-logo-bg text-black font-bold  p-2 rounded-md m-3 text-sm"
+          onClick={publishDrive}
+        >
+          Publish Drive
+        </button>
+        {/* ) : ( */}
+        <button
+          className="bg-logo-bg text-black font-bold  p-2 rounded-md m-3 text-sm"
+          onClick={unPublishDrive}
+        >
+          Unpublish Drive
+        </button>
+        {/* )} */}
+
+        <button className="bg-logo-bg text-black font-bold  p-2 rounded-md m-3 text-sm">
+          Delete Drive
+        </button>
+      </div>
+    </>
+  );
+}
