76aebdf71f38690d132d1319ae622c407f1c3694
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-05-03 18:57:20 +0530

    removed id page

--- client/src/app/profile/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import ProfileComponent from "../../../components/ProfileComponent";
-
-const ProfilePage = () => {
-    return <ProfileComponent routePrefix="profile" />;
-};
-
-export default ProfilePage;
