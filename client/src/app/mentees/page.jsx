760289169cf859b6d75663f5ca65be4839642d35
Rishal14 <125131656+Rishal14@users.noreply.github.com>
2024-05-06 22:09:59 +0530

    edited button

--- client/src/app/mentees/page.jsx
+++ client/src/app/mentees/page.jsx
@@ -21,9 +21,30 @@ export default function page() {
   };
   return (
     <div className="py-10" >
-      <button className="px-4 py-2 bg-logo-bg ml-20 text-white rounded shadow hover:bg-blue-600" onClick={handleAddMentor}>
+      <button className="px-4 py-2 bg-logo-bg ml-20 text-white rounded shadow hover:bg-green-600" onClick={handleAddMentor}>
         Add Your Mentee
       </button>
+      {isDialogOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-8 rounded shadow-lg">
+            <p className="text-lg text-black font-medium mb-4">Enter Mentee USN:</p>
+            <input
+              type="text"
+              value={newMentorUSN}
+              onChange={(e) => setNewMentorUSN(e.target.value)}
+              className="border text-black border-gray-300 rounded px-3 py-2 w-full mb-4"
+            />
+            <div className="flex justify-end">
+              <button className="px-4 py-2 bg-gray-300 text-gray-800 rounded shadow mr-2 hover:bg-gray-400" onClick={handleDialogClose}>
+                Cancel
+              </button>
+              <button className="px-4 py-2 bg-logo-bg text-white rounded shadow hover:bg-green-600" onClick={handleAddUSN}>
+                Add Mentee
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <p className="px-20 py-4 text-3xl font-medium">Your Mentees</p>
       <div className="flex flex-wrap justify-center mx-1 py-5 gap-4">
         {mentees.map((user, index) => (
@@ -32,27 +53,7 @@ export default function page() {
           </div>
         ))}
       </div>
-      {isDialogOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded shadow-lg">
-            <p className="text-lg font-medium mb-4">Enter Mentor USN:</p>
-            <input
-              type="text"
-              value={newMentorUSN}
-              onChange={(e) => setNewMentorUSN(e.target.value)}
-              className="border border-gray-300 rounded px-3 py-2 w-full mb-4"
-            />
-            <div className="flex justify-end">
-              <button className="px-4 py-2 bg-gray-300 text-gray-800 rounded shadow mr-2 hover:bg-gray-400" onClick={handleDialogClose}>
-                Cancel
-              </button>
-              <button className="px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600" onClick={handleAddUSN}>
-                Add USN
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+
     </div>
   );
 }
