545bbbfedfd130c67b2474255c1677ac446aada8
22c60.vyshnav <22c60.vyshnav@sjec.ac.in>
2024-05-14 13:04:43 +0000

    feat : Add authentication middleware for manager-only pages

---
+++ client/src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import supabase from "../src/data/supabase";
+import { getUserRole } from "./helper/get-user-role";
+export async function middleware(request: NextRequest) {
+  const path  = request.nextUrl.pathname;
+  const driveID = path.split("/")[2];
+  const isManagerOnlyPage :boolean = path === "/dashboard" || path === `/drive/${driveID}/registrations`
+  const accessToken: string = request.cookies.get("accessToken")?.value || "";
+  if(!isManagerOnlyPage){
+    return NextResponse.next();
+  }
+  if (isManagerOnlyPage && accessToken) {
+    const userRole = await getUserRole(accessToken);
+    if (userRole !== 3) {
+      // un comment this if you want to just send the unauthorized messsage and not redirected to 404 page
+
+      // return NextResponse.json(
+      //   { message: "Unauthorized Access" },
+      //   { status: 401 }
+      // );
+
+      return NextResponse.redirect(new URL("/404", request.url));
+    } else {
+      return NextResponse.next();
+    }
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard","/drive/(.*)"]
+};
