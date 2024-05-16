d337917dcf5a77f55b0dc8a17c81c64a7e61ce78
MacDsouza <120902232+MacDsouza@users.noreply.github.com>
2024-05-16 20:03:24 +0530

    redesigned header

--- client/src/components/Header2.jsx
+++ client/src/components/Header2.jsx
@@ -1,80 +1,169 @@
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { LoginContext } from "@/context";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import supabase from "@/data/supabase";
+import Headroom from "react-headroom";
 
 const Header2 = () => {
+  const router = useRouter();
+  const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
+  const { userRole, setUserRole } = useContext(LoginContext);
+
+  console.log("role", userRole);
+  //To check if the user is logged in or not
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      console.log(data);
+      if (data.session !== null) {
+        setIsLoggedIn(true);
+        console.log("loggedin");
+      } else {
+        setIsLoggedIn(false);
+        console.log("loggedout");
+      }
+    };
+    checkSession();
+  }, []);
+
+  const PathName = usePathname();
+  console.log(PathName);
+
   return (
-    <div className="navbar bg-green-500 rounded-xl w-2/3 md:mx-52 my-10">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <Headroom>
+      <div className="navbar bg-green-500 rounded-xl w-11/12  md:mx-auto my-5">
+        <div className="navbar-start">
+          <div className="dropdown">
+            { isLoggedIn ?(<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden hover:bg-green-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>): null}
+            
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-green-500 rounded-lg w-52 "
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              {PathName !== "/" ? (
+              <li>
+                <a className="hover:underline" href="/">Home</a>
+              </li>
+            ) : null}
+
+            {isLoggedIn && PathName !== "/profile" && userRole !== 3 ? (
+              <li>
+                <a className="hover:underline" href="/profile">Profile</a>
+              </li>
+            ) : null}
+
+            {userRole === 3 && PathName !== "/dashboard" ? (
+              <li>
+                <a className="hover:underline" href="/dashboard">Dashboard</a>
+              </li>
+            ) : null}
+
+            {userRole === 2 && PathName !== "/mentees" ? (
+              <li>
+                <a className="hover:underline" href="/mentees">Mentees</a>
+              </li>
+            ) : null}
+
+
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          <a
+            className="btn bg-green-500 text-white font-extrabold border-0 hover:bg-green-500 shadow-none text-2xl"
+            href="/"
           >
-            <li>
-              <a>Home</a>
-            </li>
-            {/* <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li> */}
-            <li>
-              <a>Profile</a>
-            </li>
+            SJEC
+          </a>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {PathName !== "/" ? (
+              <li>
+                <Link className="hover:bg-green-500" href="/">
+                  <button className="text-lg text-white border-b-2 border-transparent  hover:border-white transition-colors duration-200">
+                    Home
+                  </button>
+                </Link>
+              </li>
+            ) : null}
+
+            {isLoggedIn && PathName !== "/profile" && userRole !== 3 ? (
+              <li>
+                <Link className="hover:bg-green-500" href="/profile">
+                  <button className="text-lg text-white border-b-2 border-transparent hover:border-white transition-colors duration-200">
+                    Profile
+                  </button>
+                </Link>
+              </li>
+            ) : null}
+
+            {userRole === 3 && PathName !== "/dashboard" ? (
+              <li>
+                <Link className="hover:bg-green-500" href="/dashboard">
+                  <button className="text-lg text-white border-b-2 border-transparent  hover:border-white transition-colors duration-200">
+                    Dashboard
+                  </button>
+                </Link>
+              </li>
+            ) : null}
+
+            {userRole === 2 && PathName !== "/mentees" ? (
+              <li>
+                <Link className="hover:bg-green-500" href="/mentees">
+                  <button className="text-lg text-white border-b-2 border-transparent  hover:border-white transition-colors duration-200">
+                    Mentees
+                  </button>
+                </Link>
+              </li>
+            ) : null}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">SJEC</a>
+        <div className="navbar-end">
+          {PathName === "/login" ? null : isLoggedIn ? (
+            <a
+              onClick={async () => {
+                const { error } = await supabase.auth.signOut();
+                if (!error) {
+                  setIsLoggedIn(false);
+                  setUserRole(null);
+                  router.push("/");
+                }
+              }}
+              className="btn font-bold text-white hover:bg-green-500 hover:text-black "
+            >
+              Logout
+            </a>
+          ) : (
+            <a
+              className="btn font-bold text-white hover:bg-green-500 hover:text-black "
+              href="/login"
+            >
+              Login
+            </a>
+          )}
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">
-              <button>Home</button>
-            </Link>
-          </li>
-          {/* <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li> */}
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
-      </div>
-    </div>
+    </Headroom>
   );
 };
 
